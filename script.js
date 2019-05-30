ee28c6fd5c9da383522c2ff91caec36de5020d7f
Gette <37911525+AmBitSite@users.noreply.github.com>
2019-05-30 22:11:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,58 +10,59 @@ $(document).ready(function() {
         e.preventDefault();
         $.magnificPopup.close();
     });
-    for(let i=0, x = galery.length; i<=x; i++){
-        $(galery[i]).on('click', function(){
-            if($(galery[i]).hasClass('media-active')==false){
-                for(let i=0, x = galery.length; i<=x; i++){
+    for (let i = 0, x = galery.length; i <= x; i++) {
+        $(galery[i]).on('click', function() {
+            if ($(galery[i]).hasClass('media-active') == false) {
+                for (let i = 0, x = galery.length; i <= x; i++) {
                     $(galeryContain[i]).slideUp("slow");
                     $(galery[i]).removeClass('media-active');
                 };
                 $(galeryContain[i]).slideDown("slow");
                 $(galery[i]).addClass('media-active');
-            }
-            else{
+            } else {
                 $(galeryContain[i]).slideUp("slow");
                 $(galery[i]).removeClass('media-active');
             }
         });
-        
+
     }
 
-    $('.about-btn__rus').on('click',function(e){
+    $('.about-btn__rus').on('click', function(e) {
         $('.about-btn__rus').addClass('about__btn_active');
         $('.about-info__text-rus').show();
         $('.about-info__text-eng').hide();
         $('.about-btn__eng').removeClass('about__btn_active');
     });
-    $('.about-btn__eng').on('click',function(e){
+    $('.about-btn__eng').on('click', function(e) {
         $('.about-btn__eng').addClass('about__btn_active');
         $('.about-info__text-eng').show();
         $('.about-info__text-rus').hide();
         $('.about-btn__rus').removeClass('about__btn_active');
     })
-    if(window.matchMedia('(max-width: 850px)').matches){
-        $('.header-nav__btn').on('click', function(){
+    if (window.matchMedia('(max-width: 850px)').matches) {
+        $('.header-nav__btn').on('click', function() {
             $('.header-nav__btn').hide();
             $('.header-nav').slideDown('fast');
         });
-        $('.nav-list__item').on('click', function(){
+        $('.nav-list__item').on('click', function() {
             $('.header-nav').slideUp('fast');
             $('.header-nav__btn').show();
         })
     };
-    $('a[href^="#"]').click(function(){
+    $('a[href^="#"]').click(function() {
         elementClick = $(this).attr('href');
         destination = $(elementClick).offset().top;
-        $('html').animate({scrollTop:destination},1000);
-        if(window.matchMedia('(max-width: 850px)').matches){
+        $('html').animate({ scrollTop: destination }, 1000);
+        if (window.matchMedia('(max-width: 850px)').matches) {
             $('.header-nav').slideUp('fast');
             $('.header-nav__btn').show();
         }
         return false;
     });
-});
-let x = document.querySelector('.img__item_active');
- console.log(x.width);
-console.log(document.body.clientWidth);
+    let x = document.querySelector('.img__item_active');
 
+
+    // setTimeout(() => {
+    //     console.log(x.naturalHeight);
+    // }, 10000)
+});
